6fe9def8a06fe74cea0de3174b4a87f8d002c0df
Carlo Gilmar <carlogilmar12@gmail.com>
2020-03-18 10:59:27 -0600

    Adding module with defvocab

--- lib/test_vocab.ex
+++ lib/test_vocab.ex
@@ -1,18 +1,37 @@
 defmodule TestVocab do
-  @moduledoc """
-  Documentation for `TestVocab`.
-  """
+	@moduledoc """
+	Documentation for `TestVocab`.
+	"""
+	use RDF.Vocabulary.Namespace
 
-  @doc """
-  Hello world.
+	defvocab DC,
+		base_iri: "http://purl.org/dc/elements/1.1/",
+	terms: ~w[
+		contributor coverage creator date description format
+		identifier language publisher relation rights source
+		subject title type
+	]
 
-  ## Examples
+	defvocab BIBO,
+		base_iri: "http://purl.org/ontology/bibo/",
+	file: "bibo.ttl"
+	defvocab DCTERMS,
+		base_iri: "http://purl.org/dc/terms/",
+	file: "bibo.ttl"
+	defvocab EVENT,
+		base_iri: "http://purl.org/NET/c4dm/event.owl#",
+	file: "bibo.ttl"
+	defvocab FOAF,
+		base_iri: "http://xmlns.com/foaf/0.1/",
+	file: "bibo.ttl"
+	defvocab PRISM,
+		base_iri: "http://prismstandard.org/namespaces/1.2/basic/",
+	file: "bibo.ttl"
+	defvocab SCHEMA,
+		base_iri: "http://schemas.talis.com/2005/address/schema#",
+		file: "bibo.ttl"
+		defvocab STATUS,
+		base_iri: "http://purl.org/ontology/bibo/status/",
+		file: "bibo.ttl"
 
-      iex> TestVocab.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
