d968bf6b90e0007cf054cff7dce7f588d2848add
Carlo Gilmar <carlogilmar12@gmail.com>
2020-03-18 10:35:54 -0600

    First commit 🇲🇽

---
+++ lib/test_vocab.ex
@@ -0,0 +1,18 @@
+defmodule TestVocab do
+  @moduledoc """
+  Documentation for `TestVocab`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestVocab.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
